677d6f68903dd2ae3e5538a43ca180b88e8d7f6b
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-07-06 17:05:11 +0300

    created README.md

--- yatube_parsing/settings.py
+++ yatube_parsing/settings.py
@@ -1,4 +1,3 @@
-
 BOT_NAME = "yatube_parsing"
 
 SPIDER_MODULES = ["yatube_parsing.spiders"]
@@ -9,3 +8,7 @@ ROBOTSTXT_OBEY = True
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
+
+ITEM_PIPELINES = {
+    'yatube_parsing.pipelines.MondayPipeline': 300,
+}
